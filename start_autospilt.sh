3beb1f21c0afde7af41a7039d41f3101e23b61db
rainorangelemon <rainorangelemon@gmail.com>
2023-05-24 20:16:17 -0700

    clean code

--- start_autospilt.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-nohup python -u auto_split_convert_dataset.py  > log/auto_split_convert_dataset.txt 2>&1 &
--- test_MazeEnv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-nohup  python -u  main.py --env_name MazeEnv --mode test --log_time $1 --gpu_device $3 --negative_sampling just_optimal  --exp_setting MazeEnv_$2_test > log_experiment/MazeEnv_$2_test.txt 2>&1 &
-
-
-# nohup  python -u  main.py --env_name MazeEnv --mode test --log_time 1659972447 --gpu_device 0 --negative_sampling just_optimal  --exp_setting MazeEnv_justOptimal_test > log_experiment/MazeEnv_justOptimal_test.txt 2>&1 &
